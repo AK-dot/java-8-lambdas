51adc9e646bf0414dcc90bbdb912f1eb31a45c0b
Student <karhteng@gmail.com>
2023-07-20 19:07:59 +0300

    Add Iteration.java

--- src/main/java/org/example/examples/chapter3/Iteration.java
+++ src/main/java/org/example/examples/chapter3/Iteration.java
@@ -1,5 +1,63 @@
-package org.example.examples.chapter2;
+package org.example.examples.chapter3;
+
+import org.example.examples.chapter1.Artist;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class Iteration {
+
+    public int extenalCountArtistsFromLondon(List<Artist> allArtists) {
+        int count = 0;
+        for (Artist artist :
+                allArtists) {
+            if (artist.isFrom("London")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int externalCountArtistsFromLondonExpanded(List<Artist> allArtists) {
+        int count = 0;
+        Iterator<Artist> iterator = allArtists.iterator();
+        while(iterator.hasNext()) {
+            Artist artist = iterator.next();
+            if (artist.isFrom("London")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public long internalCountArtistsFromLondon(List<Artist> allArtists) {
+        long count = allArtists.stream()
+                .filter(artist -> artist.isFrom("London"))
+                .count();
+        return count;
+    }
+
+    public void filterArtistsFromLondon(List<Artist> allArtists) {
+        allArtists.stream()
+                .filter(artist -> artist.isFrom("London"));
+    }
+
+    public void filterArtistsFromLondonPrinted(List<Artist> allArtists) {
+        allArtists.stream()
+                .filter(artist -> {
+                    System.out.println(artist.getName());
+                    return artist.isFrom("London");
+                });
+    }
+
+    public long internalCountArtistsFromLondonPrinted(List<Artist> allArtists) {
+        long count = allArtists.stream()
+                .filter(artist -> {
+                    System.out.println(artist.getName());
+                    return artist.isFrom("London");
+                })
+                .count();
+        return count;
+    }
 
 }
