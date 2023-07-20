bdb76a13464845f2adedefb25b55a566d629cb80
Student <karhteng@gmail.com>
2023-07-20 23:05:44 +0300

    Add Domain Data in chapter1

---
+++ src/main/java/org/example/examples/chapter1/Perfomance.java
@@ -0,0 +1,19 @@
+package org.example.examples.chapter1;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Stream.concat;
+
+public interface Perfomance {
+
+    public String getName();
+
+    public Stream<Artist> getMusicians();
+
+    // TODO: test
+    public default Stream<Artist> getAllMusicians() {
+        return getMusicians().flatMap(artist -> {
+            return concat(Stream.of(artist), artist.getMembers());
+        });
+    }
+}
