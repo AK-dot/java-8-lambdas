78333228e8f4ba712ea2ad92bbd83d3fbfdb389e
Student <karhteng@gmail.com>
2023-08-13 18:30:36 +0300

    Add Question1.java and Question1Test.java

---
+++ src/main/java/org/example/exercises/Question1.java
@@ -0,0 +1,39 @@
+package org.example.exercises;
+
+import org.example.examples.chapter1.Album;
+import org.example.examples.chapter1.Artist;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Question1 {
+
+    public static int addUp(Stream<Integer> numbers) {
+        return numbers.reduce(0, (acc, result) -> acc + result);
+    }
+
+    public static List<String> getNamesAndOrigins(List<Artist> artists) {
+         List<String> nationality = artists.stream()
+
+                .map(artist -> artist.getNationality())
+                .collect(toList());
+
+         List<String> name = artists.stream()
+                 .map(artist -> artist.getName())
+                 .collect(toList());
+         return Stream.of( name, nationality)
+                 .flatMap(strings -> strings.stream())
+                 .collect(toList());
+    }
+
+    public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
+        return input.stream()
+                .filter(album -> album.getTrackList().size() > 2)
+                .collect(toList());
+    }
+
+
+}
---
+++ src/test/java/com/example/examples/chapter3/Question1Test.java
@@ -0,0 +1,35 @@
+package com.example.examples.chapter3;
+
+import org.example.examples.chapter1.Album;
+import org.example.examples.chapter1.SampleData;
+import org.example.exercises.Question1;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.example.examples.chapter1.SampleData.*;
+import static org.example.exercises.Question1.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Question1Test {
+
+    @Test
+    public void addUpTest() {
+        assertEquals(10, addUp(Stream.of(1, 2, 3, 4)) );
+    }
+
+    @Test
+    public void getNamesAndOriginsTest() {
+        assertEquals(Arrays.asList("John Coltrane", "US"), getNamesAndOrigins(
+                Arrays.asList(SampleData.johnColtrane)
+        ));
+    }
+
+    @Test
+    public void getAlbumsWithAtMostThreeTracksTest() {
+        assertEquals(Arrays.asList(manyTrackAlbum), getAlbumsWithAtMostThreeTracks(
+                Arrays.asList(manyTrackAlbum, sampleShortAlbum, aLoveSupreme)));
+    }
+}
