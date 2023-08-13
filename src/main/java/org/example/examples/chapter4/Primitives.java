a5a6c23419284b2a3b9f2f8177873fc94ae2db78
Student <karhteng@gmail.com>
2023-08-13 22:12:31 +0300

    Add Primitives.java

---
+++ src/main/java/org/example/examples/chapter4/Primitives.java
@@ -0,0 +1,22 @@
+package org.example.examples.chapter4;
+
+import org.example.examples.chapter1.Album;
+
+import java.util.IntSummaryStatistics;
+
+public class Primitives {
+
+    // BEGIN printTrackLengthStatistics
+    public static void printTrackLengthStatistics(Album album) {
+        IntSummaryStatistics trackLengthStats
+                = album.getTracks()
+                .mapToInt(track -> track.getLength())
+                .summaryStatistics();
+        System.out.printf("Max: %d, Min: %d, Ave: %f, Sum: %d",
+                trackLengthStats.getMax(),
+                trackLengthStats.getMin(),
+                trackLengthStats.getAverage(),
+                trackLengthStats.getSum());
+    }
+    // END printTrackLengthStatistics
+}
