f99a313e58d8c22a62e758f636f263bbcef9d140
Student <karhteng@gmail.com>
2023-08-14 16:21:55 +0300

    Add countLowercaseLetters() and mostLowercaseString()

--- src/main/java/org/example/exercises/StringExercises.java
+++ src/main/java/org/example/exercises/StringExercises.java
@@ -1,12 +1,24 @@
 package org.example.exercises;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+
+import static java.lang.Math.max;
 
 public class StringExercises {
 
     // Question 6
+    public static int countLowercaseLetters(String string) {
+        return (int) string.chars()
+                .filter(Character::isLowerCase)
+                .count();
+    }
 
     // Question 7
+    public static Optional<String> mostLowercaseString(List<String> strings) {
+        return strings.stream()
+                .max(Comparator.comparingInt(StringExercises::countLowercaseLetters));
+    }
 
 }
--- src/test/java/com/example/examples/chapter3/StringExercisesTest.java
+++ src/test/java/com/example/examples/chapter3/StringExercisesTest.java
@@ -1,4 +1,24 @@
 package com.example.examples.chapter3;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.example.exercises.StringExercises.countLowercaseLetters;
+import static org.example.exercises.StringExercises.mostLowercaseString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringExercisesTest {
+
+    @Test
+    public void countLowercaseLettersTest() {
+        assertEquals(2, countLowercaseLetters("Add"));
+    }
+
+    @Test
+    public void mostLowercaseStringTest() {
+        assertEquals(Optional.of("Sake"),
+                mostLowercaseString(Arrays.asList("Sake", "Ad", "Sun")));
+    }
 }
