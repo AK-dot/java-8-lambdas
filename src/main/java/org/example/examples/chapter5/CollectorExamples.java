8043bdaefbadd2b2410340d89383b7db9904a649
Student <karhteng@gmail.com>
2023-08-15 13:01:39 +0300

    Add methods with collectors partitioningBy() and groupingBy()

--- src/main/java/org/example/examples/chapter5/CollectorExamples.java
+++ src/main/java/org/example/examples/chapter5/CollectorExamples.java
@@ -3,13 +3,15 @@ package org.example.examples.chapter5;
 import org.example.examples.chapter1.Album;
 import org.example.examples.chapter1.Artist;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.TreeSet;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+import static java.nio.charset.Charset.defaultCharset;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.*;
 
@@ -38,4 +40,68 @@ public class CollectorExamples {
     public Map<Artist, List<Album>> albumsByArtist(Stream<Album> albums) {
         return albums.collect(groupingBy(album -> album.getMainMusician()));
     }
+
+    public Map<Artist, Integer> numberOfAlbumsDumb(Stream<Album> albums) {
+        // BEGIN NUMBER_OF_ALBUMS_DUMB
+        Map<Artist, List<Album>> albumsByArtist
+                = albums.collect(groupingBy(album -> album.getMainMusician()));
+
+        Map<Artist, Integer> numberOfAlbums = new HashMap<>();
+        for(Map.Entry<Artist, List<Album>> entry : albumsByArtist.entrySet()) {
+            numberOfAlbums.put(entry.getKey(), entry.getValue().size());
+        }
+        // END NUMBER_OF_ALBUMS_DUMB
+        return numberOfAlbums;
+    }
+
+    // BEGIN NUMBER_OF_ALBUMS
+    public Map<Artist, Long> numberOfAlbums(Stream<Album> albums) {
+        return albums.collect(groupingBy(album -> album.getMainMusician(),
+                counting()));
+    }
+    // END NUMBER_OF_ALBUMS
+
+    // BEGIN NAME_OF_ALBUMS_DUMB
+    public Map<Artist, List<String>> nameOfAlbumsDumb(Stream<Album> albums) {
+        Map<Artist, List<Album>> albumsByArtist =
+                albums.collect(groupingBy(album -> album.getMainMusician()));
+
+        Map<Artist, List<String>> nameOfAlbums = new HashMap<>();
+        for(Map.Entry<Artist, List<Album>> entry : albumsByArtist.entrySet()) {
+            nameOfAlbums.put(entry.getKey(), entry.getValue()
+                    .stream()
+                    .map(Album::getName)
+                    .collect(toList()));
+        }
+        return nameOfAlbums;
+    }
+    // END NAME_OF_ALBUMS_DUMB
+
+    // BEGIN NAME_OF_ALBUMS
+    public Map<Artist, List<String>> nameOfAlbums(Stream<Album> albums) {
+        return albums.collect(groupingBy(Album::getMainMusician,
+                mapping(Album::getName, toList())));
+    }
+    // END NAME_OF_ALBUMS
+
+    public static Map<String, Long> countWords(Stream<String> words) {
+        return words.collect(groupingBy(word -> word, counting()));
+    }
+
+    private static final Pattern SPACES = Pattern.compile("\\w+");
+
+    public static Map<String, Long> countWordsIn(Path path) throws IOException {
+        Stream<String> words = Files.readAllLines(path, defaultCharset())
+                .stream()
+                .flatMap(line -> SPACES.splitAsStream(line));
+        return countWords(words);
+    }
+
+    // BEGIN averagingTracks
+    public double averageNumberOfTracks(List<Album> albums) {
+        return albums.stream()
+                .collect(averagingInt(album -> album.getTrackList().size()));
+    }
+    // END averagingTracks
+
 }
