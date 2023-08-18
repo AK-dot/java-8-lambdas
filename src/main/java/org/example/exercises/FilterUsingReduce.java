57ed91b8c2f44778eb9d993c9775ab650de5a6e4
Student <karhteng@gmail.com>
2023-08-18 17:38:34 +0300

    Add FilterUsingReduce.java

---
+++ src/main/java/org/example/exercises/FilterUsingReduce.java
@@ -0,0 +1,30 @@
+package org.example.exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class FilterUsingReduce {
+
+    public static <I> List<I> filter(Stream<I> stream, Predicate<I> predicate) {
+        List<I> initial = new ArrayList<> ();
+        return stream.reduce(initial,
+                (List<I> acc, I x) -> {
+            if (predicate.test (x)) {
+                List<I> newAcc = new ArrayList<> (acc);
+                newAcc.add (x);
+                return newAcc;
+            } else {
+                return acc;
+            }
+                },
+                FilterUsingReduce::combineLists);
+    }
+
+    private static <I> List<I> combineLists(List<I> left, List<I> right) {
+        List<I> newLeft = new ArrayList<> (left);
+        newLeft.addAll(right);
+        return newLeft;
+    }
+}
